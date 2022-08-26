d138ad5061b36ea2504d8ea354346c90e94fe006
dl1bz <dg2dra@gmx.net>
2022-08-26 18:21:52 +0200

    correct rgr-beep

--- events.d/local/Logic-mods.tcl
+++ events.d/local/Logic-mods.tcl
@@ -2,7 +2,6 @@ namespace eval Logic {
 
 variable is_rf 0;
 variable testbetrieb 0;
-variable sql_rx_id ?;
 
 # sende k als Rogerbeep wenn HF
 proc squelch_open {rx_id is_open} {
@@ -38,7 +37,7 @@ proc send_rgr_sound {} {
     playTone 1000 200 75;
     playSilence 50;
     playTone 1000 200 150;
-    set sql_rx_id ?;
+    set sql_rx_id "?";
   }
   set is_rf 0;
 }
